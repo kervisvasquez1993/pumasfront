eb9561c49379ee660ec53265ee69aa6c561e3205
kervisvasquez24@gmail.com <kervisvasquez24@gmail.com>
2023-06-30 19:55:57 -0400

    add market in map

--- components/UI/Map/Map.jsx
+++ components/UI/Map/Map.jsx
@@ -1,15 +1,63 @@
-
-import { MapContainer, TileLayer } from "react-leaflet";
+import { useCallback, useMemo, useRef, useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import style from "./style.module.css"
+import style from "./style.module.css";
+import { icon } from "leaflet";
+
+const center = {
+  lat: 51.505,
+  lng: -0.09,
+};
 
 const Map = () => {
+  const ICON = icon({
+    iconUrl: "/images/modelo3d.png",
+    iconSize: [60, 60],
+  });
+  const [draggable, setDraggable] = useState(false);
+  const [position, setPosition] = useState(center);
+  const markerRef = useRef(null);
+  const eventHandlers = useMemo(
+    () => ({
+      dragend() {
+        const marker = markerRef.current;
+        if (marker != null) {
+          setPosition(marker.getLatLng());
+        }
+      },
+    }),
+    []
+  );
+  const toggleDraggable = useCallback(() => {
+    setDraggable((d) => !d);
+  }, []);
+
   return (
-    <MapContainer center={[52.02, 0.90]} zoom={10}>
+    <MapContainer
+      className={style.map}
+      center={center}
+      zoom={13}
+      scrollWheelZoom={true}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      ></TileLayer>
+      />
+      <Marker
+        icon={ICON}
+        draggable={draggable}
+        eventHandlers={eventHandlers}
+        position={position}
+        ref={markerRef}
+      >
+        <Popup minWidth={90}>
+          <span onClick={toggleDraggable}>
+            {draggable
+              ? "Marker is draggable"
+              : "Click here to make marker draggable"}
+          </span>
+        </Popup>
+      </Marker>
     </MapContainer>
   );
 };
--- pages/[lang]/index.js
+++ pages/[lang]/index.js
@@ -10,124 +10,122 @@ import Map from "../../components/UI/Map/index";
 // import {MapLocations} from "../../components/UI/Map/MapLocations";
 // import Map from "../../components/UI/Map/Map";
 const LangPage = ({ lang }) => {
-    console.log(lang.code, "lang");
+  console.log(lang.code, "lang");
 
-    return (
-        <Main>
+  return (
+    <Main>
+      {/* TODO:PASAR POR PROPS LOS PARAMETROS DEL BANNER */}
+      <BannerComponents />
 
-            <Map/>
-            {/* TODO:PASAR POR PROPS LOS PARAMETROS DEL BANNER */}
-            <BannerComponents />
+      <HeaderComponets
+        src="/images/fondo1.png"
+        classNameText={"py-5 colorPrimary"}
+        alignment="start"
+      >
+        CENTRO DE RESCATE Y SANTUARIO LAS PUMAS
+      </HeaderComponets>
+      <GeneralComponents
+        infoBTN={"Conoce más"}
+        colorBTN={"backgroundPrimary"}
+        reverseOrder={false}
+        srcBackgroundColor="/images/fondo1.png"
+        imageSrc="/images/venado.png"
+        image2Src="/images/modelo3d.png"
+        paragraphText={
+          "In the example below, we use the font Inter from next/font/google (you can use any font from Google or Local Fonts). Load your font with the variable option to define your CSS variable name and assign it to inter."
+        }
+      />
+      <HeaderComponets
+        src="/images/fondo1.png"
+        classNameText={"py-5 colorSecondary"}
+        alignment="end"
+      >
+        ¡CONOCE NUESTRO SANTUARIO VIRTUAL!
+      </HeaderComponets>
 
-            <HeaderComponets
-                src="/images/fondo1.png"
-                classNameText={"py-5 colorPrimary"}
-                alignment="start"
-            >
-                CENTRO DE RESCATE Y SANTUARIO LAS PUMAS
-            </HeaderComponets>
-            <GeneralComponents
-                infoBTN={"Conoce más"}
-                colorBTN={"backgroundPrimary"}
-                reverseOrder={false}
-                srcBackgroundColor="/images/fondo1.png"
-                imageSrc="/images/venado.png"
-                image2Src="/images/modelo3d.png"
-                paragraphText={
-                    "In the example below, we use the font Inter from next/font/google (you can use any font from Google or Local Fonts). Load your font with the variable option to define your CSS variable name and assign it to inter."
-                }
-            />
-            <HeaderComponets
-                src="/images/fondo1.png"
-                classNameText={"py-5 colorSecondary"}
-                alignment="end"
-            >
-                ¡CONOCE NUESTRO SANTUARIO VIRTUAL!
-            </HeaderComponets>
-
-            <GeneralComponents
-                infoBTN={"Visita virtual"}
-                colorBTN={"backgroundGris"}
-                reverseOrder={true}
-                srcBackgroundColor="/images/fondo1.png"
-                imageSrc="/images/section2.png"
-                paragraphText={
-                    "In the example below, we use the font Inter from next/font/google (you can use any font from Google or Local Fonts). Load your font with the variable option to define your CSS variable name and assign it to inter."
-                }
-            />
-            <HeaderComponets
-                src="/images/fondo1.png"
-                classNameText={"py-5 colorVerde"}
-                alignment="start"
-            >
-                NO ME SAQUES DE MI HÁBITAT
-            </HeaderComponets>
-            <GeneralComponents
-                infoBTN={"Ver más Programas"}
-                colorBTN={"backgroundVerde"}
-                reverseOrder={false}
-                srcBackgroundColor="/images/fondo1.png"
-                imageSrc="/images/nigre.png"
-                image2Src="/images/modelo3d.png"
-                paragraphText={
-                    "In the example below, we use the font Inter from next/font/google (you can use any font from Google or Local Fonts). Load your font with the variable option to define your CSS variable name and assign it to inter."
-                }
-            />
-            <HeaderComponets
-                src="/images/fondo1.png"
-                classNameText={"py-5 colorPrimary"}
-                alignment="start"
-            >
-                NOTICIAS Y BLOG
-            </HeaderComponets>
-            <Slider srcBackgroundColor={"/images/fondo1.png"} />
-            {/* <Map/> */}
-        </Main>
-    );
+      <GeneralComponents
+        infoBTN={"Visita virtual"}
+        colorBTN={"backgroundGris"}
+        reverseOrder={true}
+        srcBackgroundColor="/images/fondo1.png"
+        imageSrc="/images/section2.png"
+        paragraphText={
+          "In the example below, we use the font Inter from next/font/google (you can use any font from Google or Local Fonts). Load your font with the variable option to define your CSS variable name and assign it to inter."
+        }
+      />
+      <HeaderComponets
+        src="/images/fondo1.png"
+        classNameText={"py-5 colorVerde"}
+        alignment="start"
+      >
+        NO ME SAQUES DE MI HÁBITAT
+      </HeaderComponets>
+      <GeneralComponents
+        infoBTN={"Ver más Programas"}
+        colorBTN={"backgroundVerde"}
+        reverseOrder={false}
+        srcBackgroundColor="/images/fondo1.png"
+        imageSrc="/images/nigre.png"
+        image2Src="/images/modelo3d.png"
+        paragraphText={
+          "In the example below, we use the font Inter from next/font/google (you can use any font from Google or Local Fonts). Load your font with the variable option to define your CSS variable name and assign it to inter."
+        }
+      />
+      <HeaderComponets
+        src="/images/fondo1.png"
+        classNameText={"py-5 colorPrimary"}
+        alignment="start"
+      >
+        NOTICIAS Y BLOG
+      </HeaderComponets>
+      <Slider srcBackgroundColor={"/images/fondo1.png"} />
+      <Map />
+    </Main>
+  );
 };
 
 export default LangPage;
 const getLangs = () => {
-    const data = [
-        {
-            id: 1,
-            name: "English (en)",
-            code: "en",
-            createdAt: "2023-06-20T15:13:26.235Z",
-            updatedAt: "2023-06-20T15:13:26.235Z",
-            isDefault: false,
-        },
-        {
-            id: 2,
-            name: "Spanish (es)",
-            code: "es",
-            createdAt: "2023-06-22T18:17:52.442Z",
-            updatedAt: "2023-06-22T19:00:35.298Z",
-            isDefault: true,
-        },
-    ];
-    return data;
+  const data = [
+    {
+      id: 1,
+      name: "English (en)",
+      code: "en",
+      createdAt: "2023-06-20T15:13:26.235Z",
+      updatedAt: "2023-06-20T15:13:26.235Z",
+      isDefault: false,
+    },
+    {
+      id: 2,
+      name: "Spanish (es)",
+      code: "es",
+      createdAt: "2023-06-22T18:17:52.442Z",
+      updatedAt: "2023-06-22T19:00:35.298Z",
+      isDefault: true,
+    },
+  ];
+  return data;
 };
 export const getStaticProps = async ({ params }) => {
-    const paramId = params.lang;
-    console.log(paramId, "paramId");
-    const langs = getLangs();
-    // const lang =
-    const lang = langs.find((lang) => lang.code === paramId);
-    if (!lang) return { notFound: true };
-    return {
-        props: { lang },
-    };
+  const paramId = params.lang;
+  console.log(paramId, "paramId");
+  const langs = getLangs();
+  // const lang =
+  const lang = langs.find((lang) => lang.code === paramId);
+  if (!lang) return { notFound: true };
+  return {
+    props: { lang },
+  };
 };
 
 export const getStaticPaths = async () => {
-    const langs = getLangs();
-    const code = langs.map((lang) => lang.code);
-    const paths = code.map((path) => ({ params: { lang: path } }));
-    // console.log(langs, "langs");
-    return {
-        paths,
-        // fallback: "blocking ,
-        fallback: true,
-    };
+  const langs = getLangs();
+  const code = langs.map((lang) => lang.code);
+  const paths = code.map((path) => ({ params: { lang: path } }));
+  // console.log(langs, "langs");
+  return {
+    paths,
+    // fallback: "blocking ,
+    fallback: true,
+  };
 };
