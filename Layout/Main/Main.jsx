b0f4c7d48d7fc16c56f69f23514499c2205d2f59
kervisvasquez24@gmail.com <kervisvasquez24@gmail.com>
2023-09-18 16:58:14 -0400

    fix: update data

--- Layout/Main/Main.jsx
+++ Layout/Main/Main.jsx
@@ -69,7 +69,7 @@ const Main = ({ children, titlePage }) => {
       >
         <Navbar items={stateMenu} />
       </header>
-      <main className="">{children}</main>
+      <main className="maxWidthBody">{children}</main>
       <Footer />
     </>
   );
