090d5c96799f18e1d1c22c63e7464f621dc98eec
kervisvasquez24@gmail.com <kervisvasquez24@gmail.com>
2023-09-28 10:50:01 -0400

    feat: add pointer in jaguar

--- components/Models/Jaguar.jsx
+++ components/Models/Jaguar.jsx
@@ -5,16 +5,20 @@ import { PointLight } from "three";
 export const Jaguar = (props) => {
   const { nodes, materials } = useGLTF("/models/Jaguar.glb");
 
-  // Crea una luz
   const lightRef = useRef();
-  const intensity = 10;
+  const intensity = 10000;
 
   return (
     <group {...props} dispose={null}>
-      {/* Agrega la luz a la escena usando 'new' */}
       <pointLight
         ref={lightRef}
-        position={[0, 1, 0]}
+        position={[20, 20, 15]}
+        intensity={intensity}
+        castShadow={true}
+      />
+      <pointLight
+        ref={lightRef}
+        position={[-20, 20, 15]}
         intensity={intensity}
         castShadow={true}
       />
