143a0c53ef1c6f970cfabe85204e9b70aefd0185
kervisvasquez24@gmail.com <kervisvasquez24@gmail.com>
2023-11-06 12:36:18 -0400

    feat: add router in / for lang

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,22 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { getMenus, langAll } from '../apis/ApiBackend';
 import Loader from '../components/UI/Loader';
 
 const Home = ({ result, code }) => {
   const [firstTranslation, secondTranslation] = code;
+  const [slug, setSlug] = useState("")
+  const [lang, setLang] = useState("")
+  
   const router = useRouter();
-
+ 
   useEffect(() => {
-    const inicio = result.find((element) => element.params.slug === 'inicio')?.params.slug;
-
+    const inicio = result.find((element) => element.params.slug === 'inicio')
+    setLang(inicio.params.lang, "lang");
+    setSlug(inicio.params.slug, "slugs");
     if (inicio) {
-      const urlRedirect = `/${firstTranslation}/${inicio}`;
+      const urlRedirect = `${lang}/${slug}`;
+      console.log(urlRedirect, "urlRedirect")
       router.push(urlRedirect);
     }
   }, []);
@@ -23,15 +28,17 @@ export default Home;
 export const getStaticProps = async () => {
     const lang = await langAll();
     
-    const languages = lang.data;
-    
+    const languages = lang;
+   
     const result = [];
     const code = languages.map(element => {
-        return element.code 
+        return element.attributes.code 
     })
     for (const language of languages) {
-        const menusResponse = await getMenus(language.code);
+        const menusResponse = await getMenus(language.attributes.code);
         const menus = menusResponse.data.data;
+        console.log(menus, "menu");
+        console.log(language.attributes.code, "code")
         menus.forEach((element) => {
             result.push({
                 params: {
