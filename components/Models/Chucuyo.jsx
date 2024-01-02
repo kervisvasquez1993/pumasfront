7d969c217e06882a23698d661b777a0c073014fa
kervisvasquez1993 <kervisvasquez24@gmail.com>
2024-01-01 20:24:39 -0400

    add models in project

--- components/Models/Chucuyo.jsx
+++ components/Models/Chucuyo.jsx
@@ -2,14 +2,12 @@ import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 
 export function Chucuyo({ ...props }) {
-  
-    const { nodes, materials } = useGLTF("/models/Chucuyo.glb");
-    return (
-      <group {...props} dispose={null}>
-        <mesh geometry={nodes.Chucuyo.geometry} material={materials.amazon} />
-      </group>
-    );
-
+  const { nodes, materials } = useGLTF("/models/Chucuyo.glb");
+  return (
+    <group {...props} dispose={null}>
+      <mesh geometry={nodes.Chucuyo.geometry} material={materials.amazon} />
+    </group>
+  );
 }
 
 useGLTF.preload("/models/Chucuyo.glb");
