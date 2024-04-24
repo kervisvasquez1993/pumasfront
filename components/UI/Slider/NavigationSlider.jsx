b91e4532cdacd7b6a5a5053760c5be3d467fc7fb
kervisvasquez1993 <kervisvasquez24@gmail.com>
2024-04-24 16:03:36 -0400

    update loading en img

--- components/UI/Slider/NavigationSlider.jsx
+++ components/UI/Slider/NavigationSlider.jsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper";
 import Image from "next/image";
+import Loader from "../Loader";
 
 export default function NavigationSlider({ slides }) {
   return (
-    <Swiper navigation={true} modules={Navigation} className="mySwiper santurarioSwipper">
+    <Swiper
+      navigation={true}
+      modules={Navigation}
+      className="mySwiper santurarioSwipper"
+    >
       {slides?.map((slide, index) => {
         const { url } = slide;
+        const [isLoaded, setIsLoaded] = useState(false);
+
         return (
           <SwiperSlide key={index}>
-            <img src={url} alt={url} loading="lazy" className="conte border-0 rounded-md shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none"/>
+            {!isLoaded && (
+              <div className="flex items-center justify-center h-full">
+                <Loader />
+              </div>
+            )}
+            <img
+              src={url}
+              alt={url}
+              loading="lazy"
+              onLoad={() => setIsLoaded(true)}
+              className={`conte border-0 rounded-md shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none ${
+                isLoaded ? "" : "hidden"
+              }`}
+            />
           </SwiperSlide>
         );
       })}
