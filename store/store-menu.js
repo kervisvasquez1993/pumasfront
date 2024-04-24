60ccf3177801a1108c9464b846c307de0b5a1cfa
kervisvasquez1993 <kervisvasquez24@gmail.com>
2024-04-24 11:36:15 -0400

    update change

--- store/store-menu.js
+++ store/store-menu.js
@@ -1,21 +1,21 @@
-import create from 'zustand';
-import { ApiBackend, langAll } from "../apis/ApiBackend";
+// import create from 'zustand';
+// import { ApiBackend, langAll } from "../apis/ApiBackend";
 
-const useStore = create(set => ({
-  loading: true,
-  menuData: null,
-  footerData: [],
-  langsInfo: [],
-  whatsapp: {},
-  setLoading: (value) => set({ loading: value }),
-  setMenuData: (data, lang) => set({ menuData: { lang, data: data } }),
-  setFooterData: (ruta) => set({ footerData: ruta }),
-  setLangsInfo: (languages) => set({ langsInfo: languages }),
-  setWhatsapp: (ruta) => set({ whatsapp: ruta }),
-  getLangContext: async () => {
-    const languages = await langAll();
-    set({ langsInfo: languages });
-  },
-}));
+// const useStore = create(set => ({
+//   loading: true,
+//   menuData: null,
+//   footerData: [],
+//   langsInfo: [],
+//   whatsapp: {},
+//   setLoading: (value) => set({ loading: value }),
+//   setMenuData: (data, lang) => set({ menuData: { lang, data: data } }),
+//   setFooterData: (ruta) => set({ footerData: ruta }),
+//   setLangsInfo: (languages) => set({ langsInfo: languages }),
+//   setWhatsapp: (ruta) => set({ whatsapp: ruta }),
+//   getLangContext: async () => {
+//     const languages = await langAll();
+//     set({ langsInfo: languages });
+//   },
+// }));
 
-export default useStore;
+// export default useStore;
