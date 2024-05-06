3d5df284fc9b594017968c785edc30a8b7896df7
kervisvasquez1993 <kervisvasquez24@gmail.com>
2024-05-06 13:04:37 -0400

    refactor in slug

--- pages/[lang]/[slug]/index.js
+++ pages/[lang]/[slug]/index.js
@@ -24,6 +24,21 @@ import usePages from '../../../hooks/usePages'
 import Loader from '../../../components/UI/Loader'
 import useMenu from '../../../hooks/useMenu'
 
+const componentMap = {
+  inicio: HomePage,
+  home: HomePage,
+  nosotros: NosotrosPage,
+  history: NosotrosPage,
+  santuario: SantuarioPage,
+  sanctuary: SantuarioPage,
+  'centro-de-rescate': CentroDeRescate,
+  'rescue-center': CentroDeRescate,
+  programas: ProgramaPage,
+  programs: ProgramaPage,
+  apoyanos: ApoyanosPage,
+  'support-us': ApoyanosPage,
+}
+
 const Page = ({
   page,
   blogsPage,
@@ -57,35 +72,9 @@ const Page = ({
   }
 
   const renderContent = () => {
-    if (page.contentType === 'component') {
-      switch (page.slug) {
-        case 'inicio':
-          return <HomePage data={page} />
-        case 'home':
-          return <HomePage data={page} />
-        case 'nosotros':
-          return <NosotrosPage data={page} />
-        case 'history':
-          return <NosotrosPage data={page} />
-        case 'santuario':
-          return <SantuarioPage data={page} />
-        case 'sanctuary':
-          return <SantuarioPage data={page} />
-        case 'centro-de-rescate':
-          return <CentroDeRescate data={page} />
-        case 'rescue-center':
-          return <CentroDeRescate data={page} />
-        case 'programas':
-          return <ProgramaPage data={page} material={materialEductivoSort} />
-        case 'programs':
-          return <ProgramaPage data={page} material={materialEductivoSort} />
-        case 'apoyanos':
-          return <ApoyanosPage data={page} />
-        case 'support-us':
-          return <ApoyanosPage data={page} />
-        default:
-          return null
-      }
+    const Component = componentMap[page.slug]
+    if (Component) {
+      return <Component data={page} material={materialEductivoSort} />
     } else if (page.contentType === 'text') {
       return <p>{page.name}</p>
     } else {
@@ -94,6 +83,45 @@ const Page = ({
   }
 
   return <div>{renderContent()}</div>
+}
+
+const transformMaterialEducativo = (materialEducativodataResponse) => {
+  return materialEducativodataResponse.map((item) => {
+    const { title, description, subTitle } = item.attributes
+    const imgFile = {
+      name: item?.attributes?.imgFile?.data?.attributes?.name,
+      url: item?.attributes?.imgFile?.data?.attributes?.url,
+    }
+    const file = {
+      name: item?.attributes?.file?.data?.attributes?.name || '',
+      url: item?.attributes?.file?.data?.attributes?.url || '',
+    }
+
+    return {
+      id: item.id,
+      title,
+      description,
+      subTitle,
+      imgFile,
+      file,
+      urlExterna: item?.attributes?.urlExterna || '',
+    }
+  })
+}
+
+const transformPages = (dataPages, lang) => {
+  return dataPages.map((page) => {
+    return {
+      id: page.id,
+      title: page.attributes.title,
+      slug: page.attributes.slug,
+      componentDynamics: page?.attributes?.DynamicComponent,
+      locales: lang,
+      banner: page?.attributes?.banner,
+      contentType: 'component',
+      meta : page?.attributes?.meta
+    }
+  })
 }
 
 export const getStaticProps = async ({ params }) => {
@@ -115,49 +143,12 @@ export const getStaticProps = async ({ params }) => {
       getWhatsapp(lang),
       getMenus(lang),
     ])
-    const materialEducativodataResponse = materialEductaivo?.data?.data
-    const materialEductivoSort = []
+    const materialEductivoSort = transformMaterialEducativo(materialEductaivo?.data?.data)
     const menus = menusResponse.data.data
-    // //console.log(menus, "menus response");
     const footer = footerResponse?.data?.data[0]?.attributes?.footerInfo
     const whatsapp = whatsappResponse?.data?.data[0]?.attributes
     const pages = pagesResponse?.data?.pages
-    const dataPages = pages?.data
-    // const modelsGQ = modelsGQResponse?.data?.modelos
-    materialEducativodataResponse.forEach((item) => {
-      const { title, description, subTitle } = item.attributes
-      const imgFile = {
-        name: item?.attributes?.imgFile?.data?.attributes?.name,
-        url: item?.attributes?.imgFile?.data?.attributes?.url,
-      }
-      const file = {
-        name: item?.attributes?.file?.data?.attributes?.name || '',
-        url: item?.attributes?.file?.data?.attributes?.url || '',
-      }
-
-      const newItem = {
-        id: item.id,
-        title,
-        description,
-        subTitle,
-        imgFile,
-        file,
-        urlExterna: item?.attributes?.urlExterna || '',
-      }
-      materialEductivoSort.push(newItem)
-    })
-    const updatePage = dataPages?.map((page) => {
-      return {
-        id: page.id,
-        title: page.attributes.title,
-        slug: page.attributes.slug,
-        componentDynamics: page?.attributes?.DynamicComponent,
-        locales: lang,
-        banner: page?.attributes?.banner,
-        contentType: 'component',
-        meta : page?.attributes?.meta
-      }
-    })
+    const updatePage = transformPages(pages?.data, lang)
 
     const page = updatePage.find(
       (page) => page.locales === lang && page.slug === slug
