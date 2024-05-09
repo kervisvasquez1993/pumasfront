e3a4b844e22bfeed7cbaf20ad4e117b2d548375b
kervisvasquez1993 <kervisvasquez24@gmail.com>
2024-05-09 19:56:17 -0400

    update load data form server to clients

--- pages/[lang]/index.js
+++ pages/[lang]/index.js
@@ -1,83 +1,51 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import { getFooter, getMenus, langAll } from '../../apis/ApiBackend';
-import Loader from '../../components/UI/Loader';
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { getFooter, getMenus, langAll } from "../../apis/ApiBackend";
+import Loader from "../../components/UI/Loader";
 
-const Lang = ({ result, notFoundMessage }) => {
-    //console.log(result, "result")
-    const router = useRouter();
-    
-    if (notFoundMessage) {
-        return notFoundMessage
+const Lang = () => {
+  const [result, setResult] = useState(null);
+  const [notFoundMessage, setNotFoundMessage] = useState(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchLangAndMenus = async () => {
+      const getLangAll = await langAll();
+      const result = getLangAll.find(
+        (element) => element.attributes.code === router.query.lang
+      );
+      if (!result) {
+        setNotFoundMessage(
+          "No se encontró la ruta para el idioma proporcionado."
+        );
+      } else {
+        setResult(result);
+      }
+    };
+
+    fetchLangAndMenus();
+  }, [router.query.lang]);
+
+  if (notFoundMessage) {
+    return notFoundMessage;
+  }
+
+  let ruta;
+  if (result?.attributes?.code === "es") {
+    ruta = `/${result?.attributes?.code}/inicio`;
+  } else if (result?.attributes.code === "en") {
+    ruta = `/${result?.attributes?.code}/home`;
+  } else {
+    return notFoundMessage;
+  }
+
+  useEffect(() => {
+    if (ruta) {
+      router.push(ruta);
     }
-    //console.log(result?.code)
-    let ruta;
-    if (result?.attributes?.code === "es") {
-        ruta = `/${result?.attributes?.code}/inicio`;
-    }
-    else if (result?.attributes.code === "en") {
-        ruta = `/${result?.attributes?.code}/home`;
-    }
-    else {
+  }, [ruta]);
 
-        return notFoundMessage
-    }
-
-
-
-
-    useEffect(() => {
-
-        router.push(ruta);
-
-    }, [result]);
-
-    return <Loader />;
+  return <Loader />;
 };
 
 export default Lang;
-export const getStaticProps = async ({ params }) => {
-    const { lang } = params;
-    const getLangAll = await langAll();
-    //console.log(getLangAll, "getLangAll")
-    const result = getLangAll.find(element => element.attributes.code === lang);
-    //console.log(result, "result")
-    if (!result) {
-        return {
-            props: {
-                notFoundMessage: "No se encontró la ruta para el idioma proporcionado.",
-            },
-        };
-    }
-
-    return {
-        props: { result },
-    };
-};
-
-export const getStaticPaths = async () => {
-    const lang = await langAll();
-    const languages = lang;
-    const results = await Promise.all(
-        languages.map(async (language) => {
-          const menusResponse = await getMenus(language.attributes.code);
-          const menus = menusResponse.data.data;
-          
-          return menus.map((element) => ({
-            params: {
-              lang: element.attributes.locale,
-              slug: element.attributes.slug,
-              name: element.attributes.nombre,
-            },
-          }));
-        })
-      );
-      
-      const result = results.flat();
-
-    return {
-        paths: result,
-        fallback: true,
-    };
-};
-
