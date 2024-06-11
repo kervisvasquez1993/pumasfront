8ed3e744e90ef056e756ae53aa2f38887a38eb3f
kervisvasquez1993 <kervisvasquez24@gmail.com>
2024-06-11 11:02:44 -0400

    update data

--- pages/[lang]/about-us.js
+++ pages/[lang]/about-us.js
@@ -1,61 +1,29 @@
-'use server'
+
 import React, { useEffect } from "react";
 import { useRouter } from "next/router";
 import {
-  getAllModels,
-  getBlog,
   getMenus,
-  getModelGQ,
-  getPageWithComponents,
   getPagesGQ,
   langAll,
   getFooter,
-  getMaterialEducativo,
   getWhatsapp,
 } from "../../apis/ApiBackend";
 import usePages from "../../hooks/usePages";
 import Loader from "../../components/UI/Loader";
 import useMenu from "../../hooks/useMenu";
-import Main from "../../Layout/Main/Main";
-import ReactMarkdown from "react-markdown";
-import useScreenSize from "../../hooks/useScreenSize";
-import BannerComponents from "../../components/UI/Banner/BannerComponents";
-import HeaderComponents from "../../components/UI/HeaderComponents/HeaderComponets";
-import TwoColumnGrid from "../../components/Section/Basic/TwoColumnGrid";
-import CardBasic from "../../components/Section/CardBasic";
-import BasicSection from "../../components/Section/Basic/BasicSection";
-import ButtonView from "../../views/ButtonView";
-import Link from "next/link";
-import Slider from "../../components/UI/Slider/SliderComponts";
 import { obtenerFrase } from "../../lang/traducciones";
-import SliderTwo from "../../components/UI/Slider/SliderTwo";
-import Map from "../../components/UI/Map";
 import Head from "next/head";
 
 const Page = ({ page, footer, whatsapp, menus, meta }) => {
   const router = useRouter();
-  const { updateData } = usePages();
-  const { lang } = router.query;
-  const patrocinadores = obtenerFrase(lang, "patrocinadores");
-  const { loadedFooter, loadedWhatsapp, updateMenuLoader } = useMenu();
-  console.log(meta, "meta");
-  useEffect(() => {
-    loadedFooter(footer);
-    loadedWhatsapp(whatsapp);
-    updateMenuLoader(menus, lang);
-  }, [lang]);
-  useEffect(() => {
-    updateData(page);
-  }, [page]);
+  
+
 
   console.log(page, "page");
   if (router.isFallback) {
     return <Loader />;
   }
 
-  if (!page) {
-    return <div>Página no encontrada</div>;
-  }
 
   return (
     <>
