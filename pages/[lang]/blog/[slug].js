944ae97322443ac3b13ebaffaf80a6c883bfd795
kervisvasquez1993 <kervisvasquez24@gmail.com>
2024-06-14 16:21:39 -0400

    update data

---
+++ pages/[lang]/blog/[slug].js
@@ -0,0 +1,156 @@
+import React, { useEffect } from "react";
+import {
+  getBlog,
+  getFooter,
+  getMenus,
+  getWhatsapp,
+  langAll,
+} from "../../../apis/ApiBackend";
+import { useRouter } from "next/router";
+import Main from "../../../Layout/Main/Main";
+import HeaderComponents from "../../../components/UI/HeaderComponents/HeaderComponets";
+import SliderThree from "../../../components/UI/Slider/SliderThree";
+import ReactMarkdown from "react-markdown";
+import BasicSection from "../../../components/Section/Basic/BasicSection";
+import SliderSingle from "../../../components/UI/Slider/SliderSingle";
+import { versionInfo } from "graphql";
+import useMenu from "../../../hooks/useMenu";
+import Slider from "react-slick";
+import Image from "next/image";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { useFetch } from "../../../hooks/useFetch";
+import Head from "next/head";
+import useScreenSizeStore from "../../../store/screenSizeStore";
+const BlogInfo = ({ blog, whatsapp, footer, menus }) => {
+  const screenSize = useScreenSizeStore((state) => state.screenSize);
+  const router = useRouter();
+  const { lang } = router.query;
+  const { loadedFooter, loadedWhatsapp, updateMenuLoader } = useMenu();
+  useEffect(() => {
+    loadedFooter(footer);
+    // loadedWhatsapp(whatsapp)
+    // updateMenuLoader(menus, lang)
+  }, [lang]);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  // const {data, isLoading} = useFetch(`/api/blogs?populate=*&locale=${lang}`, {}, process.env.NEXT_PUBLIC_TOKEN)
+  // console.log(data, "data");
+  return (
+    <Main>
+      <Head>
+        <title> {blog?.attributes?.TitleBlog}</title>
+        <meta name="description" content={blog?.attributes?.ContentBlog} />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <HeaderComponents
+        classNameText={"colorPrimary chelseaFont pt-10 mt-10 lg:px-10 lg:mx-10 "}
+        alignment={`${screenSize <= 1024 ? "center" : "start"}`}
+      >
+        {blog?.attributes?.TitleBlog}
+      </HeaderComponents>
+
+      <section className="container-section py-10 my-5">
+        <section
+          className={`${blog?.attributes?.imgBlog?.data ? "flex-2" : ""}`}
+        >
+          
+
+          <div className="containerSlider">
+            <Slider {...settings}>
+              {blog?.attributes?.imgBlog?.data.map((slider) => {
+                return (
+                  <div className="w-64 h-100">
+                    <Image
+                      src={slider.attributes.url}
+                      width={450}
+                      height={450}
+                      alt="Picture of the author"
+                      className="w-full h-auto"
+                    />
+                  </div>
+                );
+              })}
+            </Slider>
+           
+          </div>
+          <section>
+            <BasicSection
+              classNameTitle={""}
+              classNameWrapper={"setionStyleTwo px-10"}
+              title={""}
+              alignItems={"center"}
+              width={`${screenSize <= 1024 ? "100%" : "100%"}`}
+              classNameContent={`${
+                screenSize <= 1024
+                  ? "align-vertical-center-horizontal-center"
+                  : "align-vertical-center-horizontal-start"
+              } fuentesParrafo `}
+            >
+              <ReactMarkdown className="contentBlog saltoLinea2">
+                {blog?.attributes?.ContentBlog}
+              </ReactMarkdown>
+            </BasicSection>
+          </section>
+        </section>
+      </section>
+    </Main>
+  );
+};
+
+export default BlogInfo;
+
+export const getStaticProps = async ({ params }) => {
+  const { lang, slug } = params;
+
+  const [blogAllResponse, whatsappResponse, footerResponse, menusResponse] =
+    await Promise.all([
+      getBlog(lang),
+      getWhatsapp(lang),
+      getFooter(lang),
+      getMenus(lang),
+    ]);
+  const whatsapp = whatsappResponse?.data?.data[0]?.attributes;
+  const footer = footerResponse?.data?.data[0]?.attributes?.footerInfo;
+
+  const blogAll = blogAllResponse.data.data;
+  const menus = menusResponse.data.data;
+
+  const blog = blogAll.find((e) => e.attributes.slug == slug);
+  return {
+    props: {
+      blog,
+      whatsapp,
+      menus,
+      footer,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const blogs = [];
+  const locales = await langAll();
+  const languages = locales;
+  for (const language of languages) {
+    const blogAllResponse = await getBlog(language.code);
+    const blogAll = blogAllResponse.data;
+    blogAll.data.forEach((element) => {
+      return blogs.push({
+        params: {
+          id: element.id.toString(),
+          body: element,
+          lang: language.code,
+        },
+      });
+    });
+  }
+  return {
+    paths: blogs,
+    fallback: true,
+  };
+};
